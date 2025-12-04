9f781254ff2f4721d8a3d818a499edcf55a9f24a
Sudhestna15 <167756953+Sudhestna15@users.noreply.github.com>
2025-12-04 21:54:36 +0530

    Update authcontroller.js

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -31,8 +31,8 @@ export const signUp = async (req, res) => {
         let token = await genToken(user._id)
         res.cookie("token", token, {
             httpOnly: true,
-            secure: false,
-            sameSite: "Strict",
+            secure: true,
+            sameSite: "none",
             maxAge: 7 * 24 * 60 * 60 * 1000
         })
         return res.status(201).json(user)
@@ -56,8 +56,8 @@ export const login = async (req, res) => {
         let token = await genToken(user._id)
         res.cookie("token", token, {
             httpOnly: true,
-            secure: false,
-            sameSite: "Strict",
+              secure: true,
+            sameSite: "none",
             maxAge: 7 * 24 * 60 * 60 * 1000
         })
         return res.status(201).json(user)
@@ -154,8 +154,8 @@ export const googleAuth = async (req, res) => {
         let token = await genToken(user._id)
         res.cookie("token", token, {
             httpOnly: true,
-            secure: false,
-            sameSite: "Strict",
+              secure: true,
+            sameSite: "none",
             maxAge: 7 * 24 * 60 * 60 * 1000
         })
         return res.status(201).json(user)
