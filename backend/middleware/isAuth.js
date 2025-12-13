245badfac424b948f95fc0d9e52ff7aaaddbba4d
Sudhestna15 <167756953+Sudhestna15@users.noreply.github.com>
2025-12-13 16:01:32 +0530

    Update isAuth.js

--- backend/middleware/isAuth.js
+++ backend/middleware/isAuth.js
@@ -1,23 +1,29 @@
-import jwt from "jsonwebtoken"
+import jwt from "jsonwebtoken";
 
-
-const isAuth = async (req, res,next) => {
+const isAuth = async (req, res, next) => {
     try {
-        let { token } = req.cookies
+        let token = req.cookies.token;
+
+        if (!token && req.headers.authorization?.startsWith("Bearer ")) {
+            token = req.headers.authorization.split(" ")[1];
+        }
+
         if (!token) {
-            return res.status(400).json({ message: "User doesnt have token" })
+            return res.status(400).json({ message: "User does not have token" });
         }
-        let verifyToken = await jwt.verify(token, process.env.JWt_SECRET)
+
+        let verifyToken = jwt.verify(token, process.env.JWT_SECRET);
+
         if (!verifyToken) {
-            return res.status(400).json({ message: "User doesnt have valid token" })
+            return res.status(400).json({ message: "User does not have valid token" });
         }
-        req.userId = verifyToken.userId
-        next()
 
+        req.userId = verifyToken.userId;
+
+        next();
     } catch (error) {
-        return res.status(500).json({ message: `isAuth error ${error}` })
-
+        return res.status(500).json({ message: `isAuth error ${error}` });
     }
-}
+};
 
-export default isAuth
+export default isAuth;
