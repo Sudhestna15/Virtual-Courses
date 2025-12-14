1f1d8c4281bb9485720fb68847a31d9ada0c131d
Sudhestna15 <167756953+Sudhestna15@users.noreply.github.com>
2025-12-14 12:00:54 +0530

    Update isAuth.js

--- backend/middleware/isAuth.js
+++ backend/middleware/isAuth.js
@@ -10,7 +10,7 @@ const isAuth = async (req, res, next) => {
 
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-    req.userId = decoded.id; // ✅ matches genToken
+    req.userId = decoded.id;
 
     next();
   } catch (error) {
